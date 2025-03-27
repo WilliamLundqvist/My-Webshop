f0841dcdee1bfba035156b5476cd2794919fb749
WilliamLundqvist <wille1000000@gmail.com>
2025-03-27 14:28:04 +0100

    redeplpuy

--- components/ui/skeleton.tsx
+++ components/ui/skeleton.tsx
@@ -1,10 +1,10 @@
-import { cn } from "@/lib/utils/utils";
-
-function Skeleton({ className, ...props }: React.ComponentProps<"div">) {
+import { cn } from '@/lib/utils/utils';
+// hello
+function Skeleton({ className, ...props }: React.ComponentProps<'div'>) {
   return (
     <div
       data-slot="skeleton"
-      className={cn("bg-primary/10 animate-pulse rounded-md", className)}
+      className={cn('bg-primary/10 animate-pulse rounded-md', className)}
       {...props}
     />
   );
